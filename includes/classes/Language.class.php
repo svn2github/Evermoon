svn2github/Evermoon

commit 99c2d3d4ecd0c9cf52ed4641d247c062a2152260

Rewrite language System (Bug #186)
git-svn-id: http://2moons.googlecode.com/svn/trunk@2496 b16e238e-0886-11df-aae1-e754eacfc418

includes/classes/Language.class.php
@@ -1,0 +1,169 @@
+<?php
+
+/**
+ *  2Moons
+ *  Copyright (C) 2012 Jan Kröpke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package 2Moons
+ * @author Jan Kröpke <info@2moons.cc>
+ * @copyright 2012 Jan Kröpke <info@2moons.cc>
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 1.7.0 (2012-12-31)
+ * @info $Id$
+ * @link http://2moons.cc/
+ */
+
+class Language implements ArrayAccess {
+    private $container = array();
+    private $langauge = array();
+    static private $allLangauges = array();
+	
+	static function getAllowedLangs($OnlyKey = true)
+	{
+		if(count(self::$allLangauges) == 0) {
+			$GLOBALS['CACHE']->add('language', 'LanguageBuildCache');
+			self::$allLangauges = $GLOBALS['CACHE']->get('language');
+		}
+		
+		if($OnlyKey) {
+			return array_keys(self::$allLangauges);
+		}
+		else {
+			return self::$allLangauges;
+		}
+	}
+	
+	public function getUserAgentLanguage()
+	{
+   		if(MODE === 'INDEX' && isset($_COOKIE['lang']) && in_array($_COOKIE['lang'], self::getAllowedLangs())) {
+			$this->setLanguage($_COOKIE['lang']);
+			return true;
+		}
+		
+   		if(isset($_REQUEST['lang']) && in_array($_REQUEST['lang'], self::getAllowedLangs())) {
+			$this->setLanguage($_REQUEST['lang']);
+			return true;
+		}
+		
+	    if (empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            return false;
+        }
+
+		$quality = 0;
+		
+		$x = explode(",", $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+		foreach ($x as $val) {
+			if(preg_match("/(.*);q=([0-1]{0,1}\.\d{0,4})/i", $val, $matches))
+			{
+				if($quality < ((float) $matches[2]))
+				{
+					$quality	= (float) $matches[2];
+					$deflang	= $matches[1];
+				}
+			}
+			else
+			{
+				$deflang = $val;
+				break;
+			}
+		}
+		
+		$this->setLanguage($deflang);
+	}
+	
+    public function __construct($langauge = NULL)
+	{
+		$this->setLanguage($langauge);
+    }
+	
+    public function setLanguage($langauge)
+	{
+		if(is_null($langauge) || !in_array($langauge, self::getAllowedLangs()))
+		{
+			$this->langauge	= Config::get('lang');
+		}
+		else
+		{
+			$this->langauge = $langauge;
+		}
+    }
+	
+    public function addData($data) {
+		$this->container = array_merge($this->container, $data);
+    }
+	
+	public function getLanguage()
+	{
+		return $this->langauge;
+	}
+	
+	public function getTemplate($templateName)
+	{
+		if(file_exists(ROOT_PATH.'language/'.$this->getLanguage().'/templates/'.$templateName.'.txt'))
+		{
+			return file_get_contents(ROOT_PATH.'language/'.$this->getLanguage().'/templates/'.$templateName.'.txt');
+		}
+		else
+		{
+			return '### Template "'.$templateName.'" on language "'.$this->getLanguage().'" not found! ###';
+		}
+	}
+	
+	public function includeData($Files)
+	{
+		// Fixed BOM problems.
+		ob_start();
+		
+        foreach($Files as $File) {
+			require(ROOT_PATH.'language/'.$this->getLanguage().'/'.$File.'.php');
+		}
+		
+		if(file_exists(ROOT_PATH.'language/'.$this->getLanguage().'/CUSTOM.php'))
+		{
+			require(ROOT_PATH.'language/'.$this->getLanguage().'/CUSTOM.php');
+		}
+		
+		ob_end_clean();
+		$this->addData($LNG);
+	}
+	
+	public function insertData($Files)
+	{
+		
+	}
+	
+	/** ArrayAccess Functions **/
+	
+    public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+	
+    public function offsetExists($offset) {
+        return isset($this->container[$offset]);
+    }
+	
+    public function offsetUnset($offset) {
+        unset($this->container[$offset]);
+    }
+	
+    public function offsetGet($offset) {
+        return isset($this->container[$offset]) ? $this->container[$offset] : $offset;
+    }
+}

install/index.php
@@ -40,13 +40,13 @@ require(ROOT_PATH . 'includes/common.php');
 
 $THEME->setUserTheme('gow');
 
-$LANG->GetLangFromBrowser();
-$LANG->includeLang(array('L18N', 'INGAME', 'INSTALL'));
+$LNG->GetLangFromBrowser();
+$LNG->includeData(array('L18N', 'INGAME', 'INSTALL'));
 
 $template = new template();
 $template->assign(array(
-	'lang'			=> $LANG->GetUser(),
-	'Selector'		=> $LANG->getAllowedLangs(false),
+	'lang'			=> $LNG->GetUser(),
+	'Selector'		=> $LNG->getAllowedLangs(false),
 	'intro_lang'	=> $LNG['intro_lang'],
 	'title'			=> $LNG['title_install'].' &bull; 2Moons',
 	'menu_intro'	=> $LNG['menu_intro'],
@@ -75,13 +75,13 @@ if (is_file($enableInstallToolFile) && (time() - filemtime($enableInstallToolFil
 }
 
 if (!is_file($enableInstallToolFile)) {
-	$template->message($LANG->getExtra('locked_install'), false, 0, true);
+	$template->message($LNG->getTemplate('locked_install'), false, 0, true);
 	exit;
 }
 
 $language	= HTTP::_GP('lang', '');
 
-if(!empty($language) && in_array($language, $LANG->getAllowedLangs()))
+if(!empty($language) && in_array($language, $LNG->getAllowedLangs()))
 {
 	setcookie('lang', $language);
 }
@@ -92,7 +92,7 @@ switch($mode)
 	case 'ajax':
 		require_once(ROOT_PATH.'includes/libs/ftp/ftp.class.php');
 		require_once(ROOT_PATH.'includes/libs/ftp/ftpexception.class.php');
-		$LANG->includeLang(array('ADMIN'));
+		$LNG->includeData(array('ADMIN'));
 		$CONFIG = array("host" => $_GET['host'], "username" => $_GET['user'], "password" => $_GET['pass'], "port" => 21); 
 		try
 		{
@@ -363,7 +363,7 @@ switch($mode)
 					Config::init();
 					Config::update(array(
 						'timezone'			=> @date_default_timezone_get(),
-						'lang'				=> $LANG->GetUser(),
+						'lang'				=> $LNG->GetUser(),
 						'OverviewNewsText'	=> $LNG['sql_welcome'].'1.7',
 						'uni_name'			=> $LNG['fcm_universe'].' 1',
 						'close_reason'		=> $LNG['sql_close_reason'],
@@ -427,7 +427,7 @@ switch($mode)
 				$SQL .= "email			= '".$DATABASE->sql_escape($AdminMail)."', ";
 				$SQL .= "email_2		= '".$DATABASE->sql_escape($AdminMail)."', ";
 				$SQL .= "ip_at_reg		= '".$_SERVER['REMOTE_ADDR']."', ";
-				$SQL .= "lang			= '".$LANG->GetUser(). "', ";
+				$SQL .= "lang			= '".$LNG->GetUser(). "', ";
 				$SQL .= "authlevel		= ".AUTH_ADM.", ";
 				$SQL .= "dpath 			= '".DEFAULT_THEME."', ";
 				$SQL .= "rights			= '', ";

commit 096a4d6c1d52675d3263af7489bfadf20fb12ff2

Installer was broken since r2496 (FIX #223; FIX #234)
git-svn-id: http://2moons.googlecode.com/svn/trunk@2506 b16e238e-0886-11df-aae1-e754eacfc418

includes/classes/Language.class.php
@@ -91,13 +91,17 @@ class Language implements ArrayAccess {
 	
     public function setLanguage($langauge)
 	{
-		if(is_null($langauge) || !in_array($langauge, self::getAllowedLangs()))
+		if(!is_null($langauge) && !in_array($langauge, self::getAllowedLangs()))
+		{
+			$this->langauge = $langauge;
+		}
+		elseif(MODE !== 'INSTALL')
 		{
 			$this->langauge	= Config::get('lang');
 		}
 		else
 		{
-			$this->langauge = $langauge;
+			$this->langauge	= DEFAULT_LANG;
 		}
     }
 	

install/index.php
@@ -40,19 +40,19 @@ require(ROOT_PATH . 'includes/common.php');
 
 $THEME->setUserTheme('gow');
 
-$LNG->GetLangFromBrowser();
+$LNG = new Language;
 $LNG->includeData(array('L18N', 'INGAME', 'INSTALL'));
 
 $template = new template();
 $template->assign(array(
-	'lang'			=> $LNG->GetUser(),
+	'lang'			=> $LNG->getLanguage(),
 	'Selector'		=> $LNG->getAllowedLangs(false),
 	'intro_lang'	=> $LNG['intro_lang'],
 	'title'			=> $LNG['title_install'].' &bull; 2Moons',
 	'menu_intro'	=> $LNG['menu_intro'],
 	'menu_install'	=> $LNG['menu_install'],
 	'menu_license'	=> $LNG['menu_license'],
-	'canUpgrade'	=> file_exists(ROOT_PATH."includes/config.php") && filesize(ROOT_PATH."includes/config.php") !== 0,
+	'canUpgrade'	=> false,#file_exists(ROOT_PATH."includes/config.php") && filesize(ROOT_PATH."includes/config.php") !== 0,
 ));
 
 $enableInstallToolFile	= ROOT_PATH.'includes/ENABLE_INSTALL_TOOL';